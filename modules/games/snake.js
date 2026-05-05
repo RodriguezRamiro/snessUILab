c743a7ddfce105b0ad2918fbd9de9fa94e0d0332
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-05 17:34:43 -0400

    declared variables to defiine direction snakebody, nextdirection, food, tilesize, cols and rows as well as move timer and interval.

--- modules/games/snake.js
+++ modules/games/snake.js
@@ -1,5 +1,18 @@
 /* //snesUILab/modules/games/snake.js */
 
+import { keys } from "../input.js";
+
+let snakeBody;
+let direction;
+let nextDirection;
+let food;
+
+let tileSize = 10;
+let cols, rows;
+
+let moveTimer = 0;
+let moveInterval = 0.1; // controls speed
+
 export const snake = {
     name: "Snake",
 
