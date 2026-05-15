f59e146620b0157c4e05ce7806668a3af52c2995
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-15 14:16:09 -0400

    moved canvas outside of stateguard to clear it outside gameplay and remove frozen behind ui overlays

--- modules/main.js
+++ modules/main.js
@@ -39,13 +39,13 @@ function update(dt){
 
 function render() {
 
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0,0,320,180);
+
     // Dont render game while in active state
     if (getState() !== STATES.GAME) {
         return;
     }
-
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0,0,320,180);
 
     renderGame(ctx);
 
