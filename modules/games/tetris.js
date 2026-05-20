0ddf38dce811d1b7444ab79022c64137bf446667
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-20 14:06:13 -0400

    initial varaibles declared verifies timing, grid rendering, cartridge lifecycle and movement architecture

--- modules/games/tetris.js
+++ modules/games/tetris.js
@@ -1,7 +1,13 @@
 /* //snesUILab/modules/games/survivalArena.js */
 
+let block ;
+
+let tileSize = 10;
+let fallTimer = 0;
+let fallInterval = 0.5;
+
 export const tetris = {
-    name: "Tetris"
+    name: "Tetris",
 
     init() {
         console.log(" Tetris starting...");
